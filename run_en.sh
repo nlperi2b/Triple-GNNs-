afab6350bdd0eda1bafacc84fda085ed36ee2e53
nlperi2b <149352593+nlperi2b@users.noreply.github.com>
2023-11-01 16:48:13 +0800

    Add files via upload

---
+++ run_en.sh
@@ -0,0 +1,5 @@
+python -u main.py \
+    --lang en \
+    --cuda_index 0 \
+    --bert_lr 1e-5 \
+    --hidden_dim 1024
---
+++ run_zh.sh
@@ -0,0 +1,5 @@
+python -u main.py \
+    --lang zh \
+    --cuda_index 0 \
+    --bert_lr 1e-5 \
+    --hidden_dim 768
